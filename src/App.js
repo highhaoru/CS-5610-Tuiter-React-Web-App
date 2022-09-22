53c6675fa6733031a683deab0918be207e8ff415
highhaoru <jin.ha@northeastern.edu>
2022-09-22 00:12:42 -0400

    added change to assignment 2

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </h1>
+        <h2>
+          This is brach for assignment 2
+        </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
