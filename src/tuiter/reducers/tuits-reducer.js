4d4463d18ee52e316d5fb167cd4b1a4c57217202
highhaoru <jin.ha@northeastern.edu>
2022-11-07 00:58:42 -0500

    final

--- src/tuiter/reducers/tuits-reducer.js
+++ src/tuiter/reducers/tuits-reducer.js
@@ -1,21 +1,5 @@
 import tuits from "../data/tuits.json";
 
-const currentUser = {
-    userName: "NASA",
-    handle: "@nasa",
-    image: "/images/hao.jpg",
-};
-
-const templateTuit = {
-    ...currentUser,
-    topic: "Space",
-    time: "2h",
-    liked: false,
-    replies: 0,
-    retuits: 0,
-    likes: 0,
-};
-
 const tuitsReducer = (state = tuits,action) => {
     switch (action.type) {
         case 'like-tuit':
@@ -64,41 +48,3 @@ const tuitsReducer = (state = tuits,action) => {
 }
 
 export default tuitsReducer;
-
-// import { createSlice } from "@reduxjs/toolkit";
-// import tuits from '../data/tuits.json';
-// const currentUser = {
-//     userName: "NASA",
-//     handle: "@nasa",
-//     image: "/images/nasa.jpeg",
-// };
-//
-// const templateTuit = {
-//     ...currentUser,
-//     topic: "Space",
-//     time: "2h",
-//     liked: false,
-//     replies: 0,
-//     retuits: 0,
-//     likes: 0,
-// };
-//
-// const tuitsSlice = createSlice({
-//                                    name: 'tuits',
-//                                    initialState: tuits,
-//                                    reducers: {
-//                                        deleteTuit(state, action) {
-//                                            const index = state.findIndex((tuit) => tuit._id === action.payload);
-//                                            state.splice(index, 1);
-//                                        },
-//                                        createTuit(state, action) {
-//                                            state.unshift({
-//                                                              ...action.payload,
-//                                                              ...templateTuit,
-//                                                              _id: new Date().getTime(),
-//                                                          });
-//                                        },
-//                                    }
-//                                });
-// export const { createTuit, deleteTuit } = tuitsSlice.actions;
-// export default tuitsSlice.reducer;
