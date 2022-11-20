e2419f2fa3911771a7a5565659dbfb60c76d632f
highhaoru <jin.ha@northeastern.edu>
2022-11-20 10:45:48 -0500

    final

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 // const API_BASE = process.env.REACT_APP_API_BASE;
 // const TUITS_API = `${API_BASE}/tuits`;
 // const TUITS_API = 'http://localhost:4000/api/tuits';
-// const TUITS_API = 'https://tuiter-node-server-app-hao-jin.herokuapp.com/api/tuits';
-const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = `${API_BASE}/tuits`;
+const TUITS_API = 'https://tuiter-node-server-app-hao-jin.herokuapp.com/api/tuits';
+// const API_BASE = process.env.REACT_APP_API_BASE;
+// const TUITS_API = `${API_BASE}/tuits`;
 
 export const findTuits = async () => {
     const response = await axios.get(TUITS_API);
